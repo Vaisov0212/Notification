0a85676d8b32cdba09cbfcdff17cb68619ed8fdf
Azamat Vaisov <azamatvaisov@Azamats-MacBook-Pro.local>
2025-07-29 11:05:08 +0500

    bootstrap template upload

--- database/migrations/2025_07_28_194121_create_remindme_table.php
+++ database/migrations/2025_07_28_194121_create_remindme_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('text');
             $table->text('description');
+            $table->
             $table->timestamps();
         });
     }
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+ <footer class="footer">
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023. Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ms-1"></i></span>
+  </div>
+</footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../../assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../../assets/js/off-canvas.js"></script>
+    <script src="../../assets/js/template.js"></script>
+    <script src="../../assets/js/settings.js"></script>
+    <script src="../../assets/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <!-- End custom js for this page-->
+  </body>
+</html>
